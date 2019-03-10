5ff68c332908dfa8adbf6e83b657380df273d030
Mayk Brito <maykbrito@gmail.com>
2019-03-10 12:54:29 -0300

    replacing superagent with axios

--- robots/wikipedia/api.js
+++ robots/wikipedia/api.js
@@ -1,24 +1,22 @@
-const superAgent =  require('superagent');
+const axios =  require('axios');
 const wikipediaApiUrl = 'https://pt.wikipedia.org/w/api.php'
 
 exports.fetchTerms = async searchTerm => {
-    const response = await superAgent.get(wikipediaApiUrl)
-    .query({
+    const response = await axios.get(wikipediaApiUrl, { params: {
         action:'opensearch',
         search: searchTerm,
         limit:5,
         namespace:0,
         format:'json'
-    })
+    }})
 
-    return response.body
+    return response.data
 }
 
 exports.fetchContent = async title => {
     console.log('Searching content...')
 
-    const response = await superAgent.get(wikipediaApiUrl)
-    .query({
+    const response = await axios.get(wikipediaApiUrl, { params: {
         action: 'query',
         prop: 'extracts|images|info|extlinks',
         redirects: 1,
@@ -26,20 +24,19 @@ exports.fetchContent = async title => {
         explaintext: true,
         titles: title,
         format: 'json'
-    })
+    }})
 
-    return response.body
+    return response.data
 }
 
 exports.fetchImageUrl = async imageTitle => {
-    const response = await superAgent.get(wikipediaApiUrl)
-    .query({
+    const response = await axios.get(wikipediaApiUrl, { params: {
         action: 'query',
         prop:   'imageinfo',
         titles: imageTitle,
         format: 'json',
         iiprop: 'url'
-    })
+    }})
 
-    return response.body
+    return response.data
 }
