d1437d840595434cf0eafd20cf00db2db7bc5072
Mayk Brito <maykbrito@gmail.com>
2019-03-18 22:38:43 -0300

    eslint refactor

--- robots/wikipedia/api.js
+++ robots/wikipedia/api.js
@@ -1,42 +1,49 @@
-const axios =  require('axios');
-const wikipediaApiUrl = 'https://pt.wikipedia.org/w/api.php'
+const axios = require('axios');
+
+const wikipediaApiUrl = 'https://pt.wikipedia.org/w/api.php';
 
 exports.fetchTerms = async searchTerm => {
-    const response = await axios.get(wikipediaApiUrl, { params: {
-        action:'opensearch',
-        search: searchTerm,
-        limit:5,
-        namespace:0,
-        format:'json'
-    }})
+  const response = await axios.get(wikipediaApiUrl, {
+    params: {
+      action: 'opensearch',
+      search: searchTerm,
+      limit: 5,
+      namespace: 0,
+      format: 'json',
+    },
+  });
 
-    return response.data
-}
+  return response.data;
+};
 
 exports.fetchContent = async title => {
-    console.log('Searching content...')
+  console.log('Searching content...');
 
-    const response = await axios.get(wikipediaApiUrl, { params: {
-        action: 'query',
-        prop: 'extracts|images|info|extlinks',
-        redirects: 1,
-        exsectionformat: 'wiki',
-        explaintext: true,
-        titles: title,
-        format: 'json'
-    }})
+  const response = await axios.get(wikipediaApiUrl, {
+    params: {
+      action: 'query',
+      prop: 'extracts|images|info|extlinks',
+      redirects: 1,
+      exsectionformat: 'wiki',
+      explaintext: true,
+      titles: title,
+      format: 'json',
+    },
+  });
 
-    return response.data
-}
+  return response.data;
+};
 
 exports.fetchImageUrl = async imageTitle => {
-    const response = await axios.get(wikipediaApiUrl, { params: {
-        action: 'query',
-        prop:   'imageinfo',
-        titles: imageTitle,
-        format: 'json',
-        iiprop: 'url'
-    }})
+  const response = await axios.get(wikipediaApiUrl, {
+    params: {
+      action: 'query',
+      prop: 'imageinfo',
+      titles: imageTitle,
+      format: 'json',
+      iiprop: 'url',
+    },
+  });
 
-    return response.data
-}
+  return response.data;
+};
