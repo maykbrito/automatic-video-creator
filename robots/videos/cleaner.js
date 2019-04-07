d2fe46300d70d4b057ff368b20af08c4d0797e43
Mayk Brito <maykbrito@gmail.com>
2019-04-07 17:18:57 -0300

    bot cleaner for video

---
+++ robots/videos/cleaner.js
@@ -0,0 +1,33 @@
+const { spawn } = require('child_process');
+
+const compress = async videoFile =>
+  new Promise((resolve, reject) => {
+    const compressor = spawn('ffmpeg', [
+      '-i',
+      `${videoFile}`,
+      `${videoFile}.mp4`,
+    ]);
+
+    compressor.stdout.on('data', data => process.stdout.write(data));
+    compressor.stderr.on('data', data => process.stdout.write(data));
+
+    compressor.on('close', () => {
+      console.log('> Compress done!');
+      resolve();
+    });
+  });
+
+const clean = async videoFile =>
+  new Promise((resolve, reject) => {
+    const cleaner = spawn('rm', ['-r', '-f', `${videoFile}`]);
+
+    cleaner.on('close', () => {
+      console.log('> Clear done!');
+      resolve();
+    });
+  });
+
+module.exports = {
+  compress,
+  clean,
+};
