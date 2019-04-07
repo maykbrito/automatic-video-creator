582b28428a6ad1f05c10ac302031e03923bb1e34
Mayk Brito <maykbrito@gmail.com>
2019-04-07 18:47:57 -0300

    large image

--- robots/images/index.js
+++ robots/images/index.js
@@ -4,7 +4,7 @@ const { downloadAllImages } = require('./download');
 const robot = async content => {
   for (const sentence of content.sentences) {
     for (const keyword of sentence.keywords) {
-      const getAllImages = await searchImages(keyword, { imgSize: 'medium' });
+      const getAllImages = await searchImages(keyword, { imgSize: 'large' });
       sentence.images.push(getAllImages.map(({ link }) => link));
     }
   }
